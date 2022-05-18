19aefc91997e6381c3338bae59ec9c46d43703a5
Evgeny Demchenko <little_pea@list.ru>
2022-05-18 21:52:36 +0800

    failing test for GeoCodingProcessor

---
+++ tests/test_geocoding.py
@@ -0,0 +1,32 @@
+import pytest
+from returns.pointfree import bind_ioresult
+from returns.unsafe import unsafe_perform_io
+
+from config.settings import Settings
+from services.geocoding import GeoCodingProcessor
+from models.point_of_interest import PointOfInterest
+
+
+
+@pytest.fixture
+def point_of_interest():
+    return PointOfInterest(
+        name='The Ukrainian House',
+        country='Poland',
+        city='Warszawa',
+        address='ul. Zamenhofa 1, 00-153',
+        lat='',
+        lng='',
+        categories="General",
+        organizations='Fundacja “Nasz Wybór”',
+        description='Crisis support center'
+    )
+
+
+def test_geocoding(point_of_interest: PointOfInterest):
+    # TODO: Make this test pass by implementing GeoCodingProcessor
+    subject = GeoCodingProcessor(Settings())
+    result = subject.enhance([point_of_interest])
+    enhanced: PointOfInterest = unsafe_perform_io(result).unwrap()[0]
+    assert enhanced.lat == '50.847608'
+    assert enhanced.lng == '16.473205'

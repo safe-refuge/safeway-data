6970411a1a3dd4833ac794aa5b1c5984a0cc99b1
Flash Hu <idisblueflash@gmail.com>
2022-05-21 16:38:27 +0800

    add translate cases

--- services/google_translate.py
+++ services/google_translate.py
@@ -20,8 +20,13 @@ class GoogleTranslateReader:
     make_request: Callable = make_request
 
     @impure_safe
-    def fetch(self, source_text: List[str]) -> dict:
+    def translate(self, source_text: List[str]) -> List[str]:
         service = build('translate', 'v2', developerKey=self.settings.developer_key)
         request: HttpRequest = service.translations().list(target='en', q=source_text)
         response = self.make_request(request)
-        return response
+        result = self.process_response(response)
+        return result
+
+    def process_response(self, response: dict) -> List[str]:
+        translations = response['translations']
+        return [translation['translatedText'] for translation in translations]
--- tests/test_translation.py
+++ tests/test_translation.py
@@ -1,10 +1,11 @@
+from typing import List
+
 import pytest
 from returns.unsafe import unsafe_perform_io
 
 from config.settings import Settings
 from services.translation import CityTranslator
 from models.point_of_interest import PointOfInterest
-
 
 
 @pytest.fixture
@@ -22,9 +23,50 @@ def point_of_interest():
     )
 
 
+@pytest.fixture()
+def point_of_interests():
+    return [PointOfInterest(city='Mladá Boleslav'), PointOfInterest(city='Warszawa')]
+
+
+@pytest.fixture()
+def poi_with_city_in_english():
+    return PointOfInterest(
+        name='Dondușeni',
+        country='',
+        city='satul Neculaieuca (Grădinița Neculaieuca )/ Neculaieuca Kindergarten ',
+        address='or. Dondușeni, str. Ștefan cel Mare, 30 (could not locate on Google maps)',
+        lat='',
+        lng='',
+        categories='Accredited Refugee Center ',
+        organizations='',
+        description='Refugee Center in a retreat center with 60 places '
+    )
+
+
+def stub_fetch_translated_text(text: List[str]) -> List[str]:
+    mapping = {
+        'Warszawa': 'Warsaw',
+        'Mladá Boleslav': 'Mlada Boleslav'}
+    return [mapping.get(t) for t in text]
+
+
 def test_translation(point_of_interest: PointOfInterest):
-    # TODO: Make this test pass by implementing CityTranslator
-    subject = CityTranslator(Settings())
+    subject = CityTranslator(Settings(), stub_fetch_translated_text)
     result = subject.translate([point_of_interest])
     enhanced: PointOfInterest = unsafe_perform_io(result).unwrap()[0]
     assert enhanced.city == "Warsaw"
+
+
+def test_translation_with_english(poi_with_city_in_english: PointOfInterest):
+    subject = CityTranslator(Settings(), stub_fetch_translated_text)
+    result = subject.translate([poi_with_city_in_english])
+    enhanced: PointOfInterest = unsafe_perform_io(result).unwrap()[0]
+    assert enhanced.city == 'Neculaieuca Kindergarten'
+
+
+def test_translations(point_of_interests: List[PointOfInterest]):
+    subject = CityTranslator(Settings(), stub_fetch_translated_text)
+    result = subject.translate(point_of_interests)
+    enhanced: List[PointOfInterest] = unsafe_perform_io(result).unwrap()
+    assert enhanced[0].city == 'Mlada Boleslav'
+    assert enhanced[1].city == 'Warsaw'

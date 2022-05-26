1c7f94d6e8963487a3db412aa401098350f94e42
murchik <murchik@mailbox.org>
2022-05-26 14:22:40 +0800

    Refactor test utils.

--- tests/spiders/test_france_red_cross.py
+++ tests/spiders/test_france_red_cross.py
@@ -1,9 +1,8 @@
 import pytest
 from scrapy import Spider
 
-from models.point_of_interest import PointOfInterest
 from config import constants
-from tests.utils import get_betamax_response
+from tests.utils import parse_betamax_response
 from scraping.spiders.france_red_cross import (
     FranceRedCrossSpider,
     parse_points,
@@ -16,11 +15,8 @@ from scraping.spiders.france_red_cross import (
 def test_parse_points(betamax_session):
     spider: Spider = FranceRedCrossSpider()
     url = spider.start_urls[0]
-    response = get_betamax_response(betamax_session, url)
-
-    points = [PointOfInterest(**point) for point in parse_points(response)]
+    points = parse_betamax_response(betamax_session, parse_points, url)
     assert points
-
     for point in points:
         assert point.name
         assert point.country
--- tests/utils.py
+++ tests/utils.py
@@ -1,22 +1,20 @@
-from typing import List
+from typing import List, Callable
 
 from requests import Session, Response
-from scrapy import Spider
 from scrapy.http import HtmlResponse
 
 from models.point_of_interest import PointOfInterest
 
 
-def parse_betamax_response(session: Session, spider: Spider, url: str) -> List[PointOfInterest]:
+def parse_betamax_response(session: Session, parse_function: Callable, url: str) -> List[PointOfInterest]:
     """
-    Loads response for a given URL through Betamax  and parses it with a given spider
+    Loads response for a given URL through Betamax
+    and parses it with a given parse function.
     """
-    scrapy_response = get_betamax_response(session, url)
-    results = [PointOfInterest(**result) for result in spider.parse(scrapy_response)]
+    response: Response = session.get(url)
+    scrapy_response = HtmlResponse(body=response.content, url=url)
+    results = [
+        PointOfInterest(**result) for result in parse_function(scrapy_response)
+    ]
 
     return results
-
-
-def get_betamax_response(session: Session, url: str) -> HtmlResponse:
-    response: Response = session.get(url)
-    return HtmlResponse(body=response.content, url=url)

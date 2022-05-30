8f82990c2518c719dc852cb07e116f4cd59d6d18
Flash Hu <idisblueflash@gmail.com>
2022-05-30 20:05:58 +0800

    make request in bulk

--- scraping/spiders/poland_rjps.py
+++ scraping/spiders/poland_rjps.py
@@ -1,11 +1,15 @@
 from typing import List
 
 import scrapy
+import json
 
+DATA_PATH = 'data/spiders/rips/'
 CATEGORY_MAPPING = {
-    'Seniors': {'category': 'Medical', 'ids_file': 'seniors.json'},
-    'Family': {'category': 'Children', 'ids_file': 'family.json'}
+    'Seniors': {'category': 'Medical', 'ids_file': f'{DATA_PATH}seniors.json'},
+    'Family': {'category': 'Children', 'ids_file': f'{DATA_PATH}family.json'}
 }
+
+DETAIL_BASE_URL = 'https://rjps.mpips.gov.pl/RJPS/WJ/wyszukiwanie/pobierzDaneJednostki.do?jednostkiIds'
 
 
 class QuotesSpider(scrapy.Spider):
@@ -14,10 +18,19 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         for key, value in CATEGORY_MAPPING.items():
             for url in self._build_urls(value['ids_file']):
-                yield scrapy.Request(url=url, callback=self.parse, cb_kwargs=value)
+                yield scrapy.Request(url=url, callback=self.parse, cb_kwargs={'category': value['category']})
 
     def _build_urls(self, file_name: str) -> List[str]:
+        ids = open_json_file(file_name)
+        return [f'{DETAIL_BASE_URL}={value["id"]}' for value in ids]
+
+    def parse(self, response, category: str):
+        import pdb;
+        pdb.set_trace()
         raise NotImplemented
 
-    def parse(self, response):
-        raise NotImplemented
+
+def open_json_file(file_name: str) -> dict:
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data

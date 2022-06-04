8532b4012d209fe6c0a842e2dd9a400cfc2bba54
Flash Hu <idisblueflash@gmail.com>
2022-06-04 11:17:58 +0800

    typo

--- tests/spiders/test_poland_rjps.py
+++ tests/spiders/test_poland_rjps.py
@@ -65,9 +65,9 @@ class TestPolandRJPSSpider:
             def _open_file(self, file_name):
                 mapping = {
                     '/foo.json': [{"id": 2321, "x": 18.59832, "y": 52.55788},
-                                 {"id": 2354, "x": 18.49855, "y": 52.998119}],
+                                  {"id": 2354, "x": 18.49855, "y": 52.998119}],
                     '/bar.json': [{"id": 2333, "x": 18.59832, "y": 52.55788},
-                                 {"id": 2336, "x": 18.49855, "y": 52.998119}]
+                                  {"id": 2336, "x": 18.49855, "y": 52.998119}]
                 }
                 return mapping.get(file_name, [])
 

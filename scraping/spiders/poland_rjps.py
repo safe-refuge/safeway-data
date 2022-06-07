05f2dc2abb6e6f8b69e637a88c87719c0f15587d
Hu Songtao <idisblueflash@gmail.com>
2022-06-07 08:41:40 +0800

    #31 WEb scraping for RJPS (#18)
    * initial
    
    * make request in bulk
    
    * use joblib to cache request
    
    * parse name
    
    * parse address
    
    * parse description
    
    * extract HTML to files for Unit Test
    
    * add category handler
    
    * support multiple categories
    
    * convert with English in description
    
    * fetch ids from remote
    
    * move default category to settings

---
+++ scraping/spiders/poland_rjps.py
@@ -0,0 +1,141 @@
+from collections import defaultdict
+from typing import List, Dict
+import requests
+import scrapy
+import json
+
+from joblib import Memory
+
+from config.constants import DEFAULT_CATEGORY
+from config.settings import Settings
+
+memory = Memory(location='cache/poland_rjps')
+settings = Settings()
+COUNTRY_NAME = 'Poland'
+
+CATEGORY_MAPPING = {
+    'Medical': ['21,22,23,24,20,19', '25,26,27,28,29,30'],
+    'Children': ['16,13,17,18,14,15', '3,4,1,2,5,6'],
+    'Disability support': ['7,8,9,10,11,12'],
+    'Mental help': ['31,32,33,34'],
+    'Social help': ['41,45,42,43,44'],
+    'Finance': ['35,36,37,38,39,40']
+}
+
+
+class PolandRJPSSpider(scrapy.Spider):
+    name = "poland_rjps"
+    DETAIL_BASE_URL = 'https://rjps.mpips.gov.pl/RJPS/WJ/wyszukiwanie/pobierzDaneJednostki.do?jednostkiIds'
+
+    def start_requests(self):
+        data = {category: self._build_urls(category_ids) for category, category_ids in CATEGORY_MAPPING.items()}
+        handler = CategoryHandler(data)
+
+        for url, _ in handler.mapping.items():
+            categories = handler.get_categories_by_url(url)
+
+            @memory.cache
+            def cached_request_with_url(url: str):
+                return scrapy.Request(url=url, callback=self.parse,
+                                      cb_kwargs={'category': categories})
+
+            yield cached_request_with_url(url=url)
+
+    def _build_urls(self, category_ids: List[str]) -> List[str]:
+        data = [self._fetch_point_ids_by_category(category_id_group) for category_id_group in category_ids]
+        ids = [item for sublist in data for item in sublist]
+        return [f'{self.DETAIL_BASE_URL}={value["id"]}' for value in ids]
+
+    def _fetch_point_ids_by_category(self, category_id_group: str):
+        return fetch_point_ids_by_category(category_id_group)
+
+    def parse(self, response, category: str):
+        return {
+            'name': self._get_name(response),
+            'country': COUNTRY_NAME,
+            'city': '',
+            'address': self._get_address(response),
+            'lat': '',
+            'lng': '',
+            'category': category or DEFAULT_CATEGORY,
+            'organizations': '',
+            'description': self._get_description(response)
+        }
+
+    def _get_name(self, response):
+        raw = response.css('h3.nazwa a::text').get()
+        return self._clean_spaces(raw)
+
+    def _clean_spaces(self, data):
+        return data.strip()
+
+    def _get_address(self, response):
+        lines = response.css('div > div > div.flex.flex-column.justify-content-center span::text').getall()
+        return ''.join(lines)
+
+    def _get_description(self, response):
+        rows = [self._get_email(response),
+                self._get_phone(response),
+                self._get_website(response),
+                self._get_update_date(response)]
+
+        return '\n'.join(map(self._clean_spaces, rows))
+
+    def _get_email(self, response):
+        return response.css('div[title=Email] > div > div::text').get() or ''
+
+    def _get_phone(self, response):
+        return response.css('div[title=Telefon] > div > span.wrap-anywhere::text').get() or ''
+
+    def _get_website(self, response):
+        return response.css('div[title="Strona www"] > div > div::text').get() or ''
+
+    def _get_update_date(self, response):
+        data = self._clean_spaces(response.css('body > div > div > div > div.data-aktualizacji::text').get()) or ''
+        return data.replace('data aktualizacji', 'updated')
+
+
+@memory.cache
+def fetch_point_ids_by_category(category_id_group: str) -> dict:
+    url = "https://rjps.mpips.gov.pl/RJPS/WJ/wyszukiwanie/zaladujDane.do"
+    payload = json.dumps({
+        "filtry": {
+            "miejscowosc": "",
+            "wojewodztwo": "",
+            "powiat": "",
+            "gmina": "",
+            "odleglosc": 0,
+            "lokalizacja": "",
+            "szukanaFraza": "",
+            "podkategorie": category_id_group,
+            "stronaListy": 1,
+            "liczbaPozycjiLista": "20",
+            "wersja": "1",
+            "widocznyPanel": "mapa",
+            "wejscieKarta": "",
+            "idJednostki": ""
+        },
+        "stronaListy": 1
+    })
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+
+    return response.json()
+
+
+class CategoryHandler:
+    def __init__(self, data: Dict[str, List[str]]):
+        self.data = data
+        self.mapping = self.get_mapping()
+
+    def get_mapping(self):
+        mapping = defaultdict(set)
+        for category, urls in self.data.items():
+            for url in urls:
+                mapping[url].add(category)
+        return mapping
+
+    def get_categories_by_url(self, url: str) -> str:
+        return ','.join(list(self.mapping[url]))
---
+++ tests/spiders/test_poland_rjps.py
@@ -0,0 +1,148 @@
+from collections import OrderedDict
+
+import pytest
+from scrapy.http import HtmlResponse
+
+from repositories.csv import open_read_only_file
+from scraping.spiders.poland_rjps import PolandRJPSSpider, CategoryHandler
+
+DATA_PATH = f'tests/spiders/data/rips'
+
+
+def build_response_with_file(file_name: str) -> HtmlResponse:
+    raw_text = open_read_only_file(f'{DATA_PATH}/{file_name}')
+    body = '\n'.join(raw_text.readlines())
+    return HtmlResponse(body=body, encoding='utf-8', url='test.com')
+
+
+@pytest.fixture
+def normal_place():
+    return build_response_with_file('normal_page.html')
+
+
+@pytest.fixture
+def missing_info_place():
+    return build_response_with_file('missing_info_page.html')
+
+
+class TestPolandRJPSSpider:
+    def test_parse_name(self, normal_place):
+        name = PolandRJPSSpider().parse(normal_place, category='test').get('name')
+        assert name == 'Miejski Ośrodek Pomocy Społecznej w Zabłudowie'
+
+    def test_parse_address(self, normal_place):
+        address = PolandRJPSSpider().parse(normal_place, category='test').get('address')
+
+        assert address == '16-060 Zabłudów ul. Rynek 8'
+
+    def test_parse_email(self, normal_place):
+        description = PolandRJPSSpider().parse(normal_place, category='test').get('description')
+        assert 'biuro@mops-zabludow.pl' in description
+
+    def test_parse_phone(self, normal_place):
+        description = PolandRJPSSpider().parse(normal_place, category='test').get('description')
+        assert 'tel. 85 7188100' in description
+
+    def test_parse_website(self, normal_place):
+        description = PolandRJPSSpider().parse(normal_place, category='test').get('description')
+        assert 'http://bip.mops.um.zabludow.wrotapodlasia.pl' in description
+
+    def test_parse_update_date(self, normal_place):
+        description = PolandRJPSSpider().parse(normal_place, category='test').get('description')
+        assert 'updated: 2018-02-05' in description
+
+    def test_parse_missing_info_page(self, missing_info_place):
+        description = PolandRJPSSpider().parse(missing_info_place, category='test').get('description')
+        assert 'updated: 2013-12-13' in description
+
+    def get_spider(self):
+        class StubPolandRJPSSpider(PolandRJPSSpider):
+            DETAIL_BASE_URL = 'https://test.com/?id'
+
+            def _fetch_point_ids_by_category(self, category_id_group):
+                mapping = {
+                    '21,22,23,24,20,19': [{"id": 2321, "x": 18.59832, "y": 52.55788},
+                                          {"id": 2354, "x": 18.49855, "y": 52.998119}],
+                    '25,26,27,28,29,30': [{"id": 2333, "x": 18.59832, "y": 52.55788},
+                                          {"id": 2336, "x": 18.49855, "y": 52.998119}]
+                }
+                return mapping.get(category_id_group, [])
+
+        return StubPolandRJPSSpider()
+
+    def test_build_url_from_one_file(self):
+        spider = self.get_spider()
+        urls = spider._build_urls(['21,22,23,24,20,19'])
+        assert urls == ['https://test.com/?id=2321', 'https://test.com/?id=2354']
+
+    def test_build_url_from_more_files(self):
+        spider = self.get_spider()
+        urls = spider._build_urls(['21,22,23,24,20,19', '25,26,27,28,29,30'])
+        assert urls == ['https://test.com/?id=2321', 'https://test.com/?id=2354',
+                        'https://test.com/?id=2333', 'https://test.com/?id=2336']
+
+
+class TestCategoryHandler:
+    def get_handler(self, data):
+        class StubCategoryHandler(CategoryHandler):
+            GET_MAPPING_TIMES = 0
+
+            def get_mapping(self):
+                self.GET_MAPPING_TIMES += 1
+                return super().get_mapping()
+
+        return StubCategoryHandler(data)
+
+    def test_single_category(self):
+        data = {'Medical': ['https://foo.com/123', 'https://foo.com/126']}
+        handler = self.get_handler(data)
+        assert handler.get_mapping() == {'https://foo.com/123': {'Medical'}, 'https://foo.com/126': {'Medical'}}
+
+    def test_two_categories(self):
+        data = {
+            'Medical': ['https://foo.com/123', 'https://foo.com/126'],
+            'Children': ['https://foo.com/123', 'https://foo.com/129']}
+        handler = self.get_handler(data)
+        assert handler.get_mapping() == {'https://foo.com/123': {'Medical', 'Children'},
+                                         'https://foo.com/126': {'Medical'},
+                                         'https://foo.com/129': {'Children'}, }
+
+    def test_two_categories_duplicated_urls(self):
+        data = {
+            'Medical': ['https://foo.com/123', 'https://foo.com/126', 'https://foo.com/126'],
+            'Children': ['https://foo.com/123', 'https://foo.com/129']}
+        handler = self.get_handler(data)
+        assert handler.get_mapping() == {'https://foo.com/123': {'Medical', 'Children'},
+                                         'https://foo.com/126': {'Medical'},
+                                         'https://foo.com/129': {'Children'}, }
+
+    def test_three_categories(self):
+        data = {
+            'Medical': ['https://foo.com/123', 'https://foo.com/126'],
+            'Children': ['https://foo.com/123', 'https://foo.com/129'],
+            'People with disabilities': ['https://foo.com/133', 'https://foo.com/129']}
+        handler = self.get_handler(data)
+        assert handler.get_mapping() == {'https://foo.com/123': {'Medical', 'Children'},
+                                         'https://foo.com/126': {'Medical'},
+                                         'https://foo.com/129': {'Children', 'People with disabilities'},
+                                         'https://foo.com/133': {'People with disabilities'}}
+
+    def test_get_category_by_url(self):
+        data = {'Medical': ['https://foo.com/123', 'https://foo.com/126']}
+        handler = self.get_handler(data)
+        assert handler.get_categories_by_url('https://foo.com/123') == 'Medical'
+
+    def test_get_categories_by_url(self):
+        data = OrderedDict({'Medical': ['https://foo.com/123', 'https://foo.com/126'],
+                            'Children': ['https://foo.com/123']})
+        handler = self.get_handler(data)
+        categories = handler.get_categories_by_url('https://foo.com/123')
+        assert 'Children' in categories
+        assert 'Medical' in categories
+
+    def test_build_mapping_once(self):
+        data = {'Medical': ['https://foo.com/123', 'https://foo.com/126']}
+        handler = self.get_handler(data)
+        handler.get_categories_by_url('https://foo.com/123')
+        handler.get_categories_by_url('https://foo.com/123')
+        assert handler.GET_MAPPING_TIMES == 1
